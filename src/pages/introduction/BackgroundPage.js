da894daa8e29824a8bad386611a1e1b1cfcc5f4c
Christopher Kimoliatis <ckimoliatis1@gmail.com>
2025-11-17 14:40:16 -0800

    added backgound

--- src/pages/introduction/BackgroundPage.js
+++ src/pages/introduction/BackgroundPage.js
@@ -6,9 +6,14 @@ const BackgroundPage = () => {
     <main className="page-container">
       <section className="longform longform-wide">
         <h1 className="longform-title">Background Information</h1>
-        <p className="text-content">
-          Provide essential context, definitions, and framing for the topic here. Outline key terms,
-          scope, and any prior conditions that help readers understand the narrative that follows.
+        <p className="text-content text-justify">
+          For our research project, we aim to examine how education outcomes differ across multiple marginalized communities. Specifically, we investigate performance disparities in mathematics and reading among elementary and middle school students for different racial, socioeconomic, and gender groups.
+        </p>
+        <p className="text-content text-justify">
+          In addition, we seek to understand how these communities were affected by major world crises, specifically the COVID-19 pandemic and its shift to online learning. Through this lens, we hope to determine whether such disruptions widened existing educational gaps. If so, how can we recommend policies that ensure stability over periods of world crises? And if not, how can we recommend policies that resolve differences that are present regardless of whether world crises are present or not?
+        </p>
+        <p className="text-content text-justify">
+          Through our project, we hope to help educational stakeholders better tailor policies that minimize this gap in education.
         </p>
 
         <div className="next-section">

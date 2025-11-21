b392bdad05f7aeda39a62afadad3e2cf713ce13c
Christopher Kimoliatis <111836284+CKimoliatis@users.noreply.github.com>
2025-11-21 12:14:59 -0800

    fixed data critique

--- src/pages/data/DataCritiquePage.js
+++ src/pages/data/DataCritiquePage.js
@@ -6,11 +6,6 @@ const DataCritiquePage = () => {
     <main className="page-container">
       <section className="longform longform-wide">
         <h1 className="longform-title">Data Critique</h1>
-
-        <p className="text-content">
-          For our project, we chose to work with two datasets from the Stanford Education Data Archive (SEDA):  the “Administrative District Annual Subgroup 2024.2” dataset and the “Geographic District Long 5.0” dataset. Both datasets were developed by the Educational Opportunity Project (EOP) at Stanford University. Additionally, we saw that this project is supported by a grant from the Gates Foundation and uses source data from Zelma, NCES, along with the National Assessment Governing Board. The first dataset, “Administrative District Annual Subgroup 2024.2,” provides summaries of average math and reading scores across student subgroups defined by gender, race, and economic status. The dataset covers the years 2009 through 2024, allowing analysis of educational trends during and after the COVID-19 recovery period. The second dataset, “Geographic District Long 5.0,” focuses on the earlier period 2009–2019 and reports test score data by district, grade, year, and subject (Math or English/Language Arts). Each record represents a geographic district, including average academic performance and achievement gaps disaggregated by year, grade, and subject, allowing researchers to examine patterns of educational inequality.
-        </p>
-
         <p className="text-content">
 ​​          The SEDA dataset is separated into variables that allow us to compare student performance. First, this data is designed to track academic gaps. We can calculate the difference between average scores for Black and white students, for students from low-income families and higher-income families, and for male and female students. Second, since the data contains a period of data from 2009 to 2024, it shows historical trends as well. We can analyze and see if these gaps have been changing or staying the same over the years. Finally, the separation by school district illuminates geographic inequality. We can identify academic gaps across the entire country to see if they vary significantly by region. This allows us to identify where gaps are unusually small or where they are abnormally large.
         </p>
